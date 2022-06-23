fb6ac525c2881603cfe7c23d178a5710595be0cb
Roel Damalerio <damalerioroel@gmail.com>
2022-06-23 14:36:56 +0800

    bug from app.blade.ph

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -39,7 +39,7 @@
     <!-- Control Sidebar -->
     <aside class="control-sidebar control-sidebar-dark">
         <!-- Control sidebar content goes here -->
-        {{}}
+   
     </aside>
     <!-- /.control-sidebar -->
 
