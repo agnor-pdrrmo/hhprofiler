552fea23ac575d6c6831dae7bc4c1bac0ea9b6ef
Roel Damalerio <damalerioroel@gmail.com>
2022-07-01 04:40:40 +0800

    Update welcome page

---
+++ resources/js/components/AvailprogramesComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="myGroup">   
+          <ul>
+            <li v-for="program in programs" :key="program.id">
+              <a style="text-transform: capitalize" data-toggle="collapse" :href="'#program-' + program.id" aria-expanded="false" aria-controls="collapseExample">
+                  {{program.surname}} {{program.firstname}} {{program.middlename}}
+              </a>
+              <div class="collapse" :id="'program-' + program.id" data-parent="#myGroup">
+                  <program-form-component :program="program" :submit="'Update'"></program-form-component>
+              </div>
+            </li>        
+          </ul>  
+  </div>  
+</template>
+<script>
+
+import $ from 'jquery';
+
+export default {
+  props : ['programs'],
+  data(){
+     return{
+      
+     }
+  }
+}
+</script>
